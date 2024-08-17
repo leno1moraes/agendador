318ed48429519ee343f3fbafec86fc536b6663b4
leno1moraes <lenomoraes12@gmail.com>
2024-08-17 00:48:00 -0400

    atualizar backend

--- backend/scheduler/src/main/java/com/backend/leno/scheduler/controller/RecordController.java
+++ backend/scheduler/src/main/java/com/backend/leno/scheduler/controller/RecordController.java
@@ -2,8 +2,11 @@ package com.backend.leno.scheduler.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.backend.leno.scheduler.model.Record;
 
 import com.backend.leno.scheduler.service.RecordService;
 
@@ -21,5 +24,10 @@ public class RecordController {
 	@GetMapping
 	public String testarGet() {
 		return "Olá Mundo";
+	}
+	
+	@PostMapping
+	public Record save(@RequestBody Record record) {
+		return recordService.save(record);
 	}
 }
