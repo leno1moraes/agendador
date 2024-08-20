664b30921e6b7255f6e8c4a0ed9921a1853568b3
leno1moraes <lenomoraes12@gmail.com>
2024-08-20 01:02:48 -0400

    atualizar backend

--- backend/scheduler/src/main/java/com/backend/leno/scheduler/controller/RecordController.java
+++ backend/scheduler/src/main/java/com/backend/leno/scheduler/controller/RecordController.java
@@ -1,10 +1,15 @@
 package com.backend.leno.scheduler.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.backend.leno.scheduler.model.Record;
 
@@ -22,9 +27,21 @@ public class RecordController {
 	}
 	
 	@GetMapping
-	public String testarGet() {
-		return "Olá Mundo";
+	public Page<Record> findPaginated(@RequestParam(name = "service", required = false) String service,
+            @RequestParam(name = "customer", required = false) String customer,
+            @RequestParam(name = "location", required = false) String location,
+            @RequestParam(name = "initialDate", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate initialDate,
+            @RequestParam(name = "finalDate", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate finalDate,
+            @RequestParam(name = "done", required = false) Boolean done,
+            @RequestParam(name = "canceled", required = false) Boolean canceled,
+            @RequestParam(name = "page", defaultValue = "10") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size) {
+		
+		return recordService.findPaginated(service, customer, location, initialDate, finalDate, canceled, done, page, size);
 	}
+	
 	
 	@PostMapping
 	public Record save(@RequestBody Record record) {
--- backend/scheduler/src/main/java/com/backend/leno/scheduler/service/RecordService.java
+++ backend/scheduler/src/main/java/com/backend/leno/scheduler/service/RecordService.java
@@ -1,19 +1,33 @@
 package com.backend.leno.scheduler.service;
 
+import com.backend.leno.scheduler.repository.RecordCustomRepository;
 import com.backend.leno.scheduler.repository.RecordRepository;
 import com.backend.leno.scheduler.model.Record;
+
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecordService {
 	
 	private final RecordRepository repository;
+	private final RecordCustomRepository customRepository;
 	
-	public RecordService(RecordRepository repository) {
+	public RecordService(RecordRepository repository, RecordCustomRepository customRepository) {
 		this.repository = repository;
+		this.customRepository = customRepository;
 	}
 	
 	public Record save(Record record) {
 		return repository.save(record);
 	}
+	
+	
+	public Page<Record> findPaginated(String service, String customer, String location, LocalDate initialDate,
+			LocalDate finalDate, Boolean canceled, Boolean done, int page, int size){
+		return customRepository.findPaginated(service, customer, location, initialDate, finalDate, canceled, done, page, size);
+	}
+	
 }
